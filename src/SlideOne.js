6f3ab35d61ee2c1a1c8636a39547ee08370ed846
Todd Cermak <tcermak@bloomhealthco.com>
2017-11-30 15:13:26 -0600

    Commit

---
+++ src/SlideOne.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+const SlideOne = (props) => {
+
+  // let background = {
+  //   backgroundImage: 'url(minneapolis.jpg)',
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center'
+  // };
+
+  return <div className="slide">Slide #1</div>
+}
+
+export default SlideOne;
